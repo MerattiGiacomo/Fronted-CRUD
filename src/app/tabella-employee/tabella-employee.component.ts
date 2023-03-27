1325481baf38e202bf23f834aae2553f91bb261f
lai2-01 <lai2-01@itismeucci.com>
2023-03-27 11:33:39 +0200

    base folder created

---
+++ src/app/tabella-employee/tabella-employee.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabella-employee',
+  templateUrl: './tabella-employee.component.html',
+  styleUrls: ['./tabella-employee.component.css']
+})
+export class TabellaEmployeeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
